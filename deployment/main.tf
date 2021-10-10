6e42895bfaa2031954ce22d7b53a5061244c6563
boebeng <info@flex-ci.de>
2021-10-10 17:13:45 +0200

    Add Terraform workflow

---
+++ deployment/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket         = "boebeng-flexci-global"
+    key            = "flexci-demo-"
+    region         = "eu-central-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
