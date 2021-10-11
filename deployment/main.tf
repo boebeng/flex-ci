ef16fb2c59be214697bee89b267e0a2878eb3192
boebeng <info@flex-ci.de>
2021-10-11 07:27:06 +0200

    revert previous change in main TF script

--- deployment/main.tf
+++ deployment/main.tf
@@ -20,39 +20,5 @@ provider "aws" {
   profile = "default"
   region  = "eu-central-1"
 }
-resource "aws_amplify_app" "amplify_app" {
-  name         = "flexci-ampl-test"
-  repository   = "https://github.com/boebeng/flex-ci.git"
-  access_token = var.REPO_TOKEN
 
-  # The default build_spec added by the Amplify Console for React.
-  build_spec = <<-EOT
-    version: 0.1
-    frontend:
-      phases:
-        preBuild:
-          commands:
-            - yarn install
-        build:
-          commands:
-            - echo Here is my build
-      artifacts:
-        baseDirectory: ./
-        files:
-          - '**/*'
-      cache:
-        paths:
-          - node_modules/**/*
-  EOT
 
-  # The default rewrites and redirects added by the Amplify Console.
-  custom_rule {
-    source = "/<*>"
-    status = "404"
-    target = "/index.html"
-  }
-
-  environment_variables = {
-    ENV = "develop"
-  }
-}
